4c85f2f1eb85049b6318ac4a3977ce0ca4d37d48
elpaso <apasotti@gmail.com>
2015-04-10 12:19:36 +0200

    Added CSS GetFeatureInfo filter

---
+++ filters/GetFeatureInfoCSSFilter.py
@@ -0,0 +1,46 @@
+
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    QGIS Server Plugin Filters: this test filter adds a CSS to HTML
+    get feature info response.
+    ---------------------
+    Date                 : October 2014
+    Copyright            : (C) 2014-2015 by Alessandro Pasotti
+    Email                : apasotti at gmail dot com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+import os
+
+from qgis.server import *
+from qgis.core import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+
+class GetFeatureInfoCSSFilter(QgsServerFilter):
+
+    def __init__(self, serverIface):
+        super(GetFeatureInfoCSSFilter, self).__init__(serverIface)
+
+    def responseComplete(self):
+        request = self.serverInterface().requestHandler()
+        params = request.parameterMap( )
+        if (params.get('SERVICE').upper() == 'WMS' \
+                and params.get('REQUEST', '').upper() == 'GETFEATUREINFO' \
+                and params.get('INFO_FORMAT', '').upper() == 'TEXT/HTML' \
+                and not request.exceptionRaised() ):
+            body = request.body()
+            body.replace('<BODY>', """<BODY><STYLE type="text/css">* {font-family: arial, sans-serif; color: blue;}</STYLE>""")
+            # Set the body
+            request.clearBody()
+            request.appendBody(body)
